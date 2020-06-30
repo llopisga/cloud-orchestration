207b631f29be18c21e19e8e07c007fbe5991133c
Raul Ll <raullg8@gmail.com>
2020-06-30 19:01:35 +0200

    Adding thesis guides

--- PSM/repositories/antidote/provision.sh
+++ PSM/repositories/antidote/provision.sh
@@ -3,31 +3,34 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-# Setting global variables to arguments
+# The main variables are assigned using the arguments of the code call
 VMPROVIDER=$1
 DEPLOYPATH=$2
 UUID=$3
 PORT=$4
 
-# Go to the antidote-selfmedicate path
+# Change directory to the antidote-selfmedicate path
 cd $DEPLOYPATH
 
+# In this implementation, libvirt has not been introduced, although work has been done so the code is left.
+
 if [ "$VMPROVIDER" == "virtualbox" ]; then
-    # Size is equal to small, we are going to use Virtualbox
-
-
+    # If the provider is Virtualbox
+     
+    # Check if Virtualbox and vagrant are installed. Abort if not.
     command -v VBoxManage >/dev/null 2>&1 || { echo >&2 "I require VBoxManage but it's not installed.  Aborting."; exit 1; }
     command -v vagrant >/dev/null 2>&1 || { echo >&2 "I require vagrant but it's not installed.  Aborting."; exit 1; }
     
-    # Change directory to antidote selfmedicate and run up the Vagrant provisioner.
+    # Change directory to antidote selfmedicate and install needed plugins
     cd ./antidote-selfmedicate
     
     vagrant plugin install vagrant-vbguest
     vagrant plugin install vagrant-hostsupdater
 
 elif [ "$VMPROVIDER" == "libvirt" ]; then
-    # Size is equal to medium or large, we are going to use Docker
+    # If the provider is Libvirt
 
+    # Check if Libvirt and vagrant are installed. Abort if not.
     command -v libvirtd >/dev/null 2>&1 || { echo >&2 "I require libvirtd but it's not installed.  Aborting."; exit 1; }
     command -v vagrant >/dev/null 2>&1 || { echo >&2 "I require vagrant but it's not installed.  Aborting."; exit 1; }
     
@@ -36,19 +39,20 @@ elif [ "$VMPROVIDER" == "libvirt" ]; then
     vagrant plugin install vagrant-libvirt
     vagrant plugin install vagrant-hostsupdater
 
-    #service libvirtd start
-
 fi
 
-# Generate output completion
-#COMMAND='vagrant up 2> ./exited_error.txt
+# Be sure to be in Antidote dir and perform the deployment of the Antidote code
+cd ./antidote-selfmedicate
 vagrant up
 
+# Add designed port to firewall exception
 sudo firewall-cmd --permanent --add-port=$PORT/tcp
 sudo firewall-cmd --reload
 
+# Change working directory to the parent deploy directory
 cd $DEPLOYPATH
 
+# Create the instructions for the teacher if the service succeeds
 echo "The service with UUID $UUID, is ready at URL antidote-local:$PORT." >> completed.txt
 echo "Before accessing it, add this line:" >> completed.txt
 echo "Linux: gedit /etc/hosts" >> completed.txt
--- PSM/repositories/jupyter/provision.sh
+++ PSM/repositories/jupyter/provision.sh
@@ -9,46 +9,56 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 
-# Setting global variables to arguments
+# The main variables are assigned using the arguments of the code call
 DEPLOYPATH=$1
 LESSON=$2
 TYPE=$3
 
-# Go to the antidote-selfmedicate path
+# Change directory to the antidote-selfmedicate path
 cd $DEPLOYPATH
 
-# if dir not exists
+# If the main Jupyter code is not present, then clone it
 if [ ! -d "the-littlest-jupyterhub" ]
 then
     git clone https://github.com/jupyterhub/the-littlest-jupyterhub.git
 fi
+
+# Jump to the Jupyter code directory
 cd the-littlest-jupyterhub/
 
+# Build the docker image and run it with variables
 docker build -t tljh-systemd . -f integration-tests/Dockerfile
 docker run --privileged --detach --name=JUPYTER_VMNAME --publish JUPYTER_PORT:80 --mount type=bind,source=$(pwd),target=/srv/src tljh-systemd
+# After being created, first execute the installation and then set resources limits
 docker exec -t JUPYTER_VMNAME python3 /srv/src/bootstrap/bootstrap.py --admin admin:JUPYTER_PASSWORD
-
 docker exec -t JUPYTER_VMNAME sudo tljh-config set limits.memory JUPYTER_MEMORYM
 docker exec -t JUPYTER_VMNAME sudo tljh-config set limits.cpu JUPYTER_CPUS
 
+# For N users requested, create them in the system
 for i in {1..JUPYTER_USERS}
 do
     docker exec -t JUPYTER_VMNAME sudo tljh-config add-item users.allowed user$i
 done
 
 # Future work: All info in tljh/configurer.py
+# Add collaboration modes
 #if groups:
     #docker exec -t JUPYTER_VMNAME sudo tljh-config set users.extra_user_groups.group1 user1
 
-
+# Reload all applied config in TLJH
 docker exec -t JUPYTER_VMNAME sudo tljh-config reload
 
+# Clone the selected lesson in the shared folder under /home
 docker exec -t JUPYTER_VMNAME git clone $LESSON /home/shared/$TYPE
+
+# Add designed port to firewall exception
 sudo firewall-cmd --permanent --add-port=JUPYTER_PORT/tcp
 sudo firewall-cmd --reload
 
-
+# Change working directory to the parent deploy directory
 cd $DEPLOYPATH
+
+# Create the instructions for the teacher if the service deployment succeeds
 echo "The service with UUID JUPYTER_VMNAME, is ready at URL $(eval "hostname -i"):JUPYTER_PORT." >> completed.txt
 echo "Administrator credentials:" >> completed.txt
 echo "Username: admin" >> completed.txt
